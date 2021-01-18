1ea6a38da6aea17015381192caf9663392311707
pingkuri <zuzupython@gmail.com>
2021-01-18 22:42:40 +0900

    fix readme

--- 123.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-
-with urlopen('https://datalab.naver.com/keyword/realtimeList.naver?where=main') as response:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    print(soup)
